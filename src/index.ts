be2cf49cd1c2691c2c6917bc23dadcb6d4a64f57
김유진 <euginy89@gmail.com>
2020-03-09 23:03:22 +0900

    test

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+require('module-alias/register');
+const { port, env } = require('@infra/vars');
+const logger = require('@infra/logger');
+const app = require('@infra/express');
+const mysql = require('@infra/mysql');
+
+const bootstrap = async () => {
+  await mysql.connect();
+  app.listen(port, () => {
+    logger.info(`server started on port ${port} (${env})`);
+  });
+};
+//asdfasfasfasfasf
+bootstrap();
+
+/**
+ * Exports express
+ * @public
+ */
+module.exports = app;
